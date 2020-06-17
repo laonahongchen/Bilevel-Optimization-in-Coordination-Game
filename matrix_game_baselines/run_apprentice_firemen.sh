4f082c217125c0ab9672c02c2798a612478952bf
Logwin <logwinhuang@126.com>
2020-06-17 11:17:55 +0800

    add matrix game baselines

--- matrix_game_baselines/run_apprentice_firemen.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..1};
-do
- python3 apprentice_firemen.py --environment ApprenticeFiremen_V2_C10_FS --processor '/gpu:0'
-done
--- matrix_game_baselines/run_cmotp.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..1};
-do
- python cmotp.py --environment CMOTP_V1 --processor '/gpu:0' --madrl hysteretic
-done
